bd13589368f17cbfe82c624dccbc6deaddf8af97
Hafsa FAIZ <h.faiz0872@uca.ac.ma>
2026-04-10 15:25:29 +0100

    save registration progress

---
+++ user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.example.user_service.controller;
+
+
+import com.example.user_service.model.User;
+import com.example.user_service.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return service.create(user);
+    }
+
+    @GetMapping
+    public List<User> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getById(@PathVariable Long id) {
+        return service.getById(id);
+    }
+}
---
+++ user-service/src/main/java/com/example/user_service/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.user_service.service;
+
+
+import com.example.user_service.model.User;
+import com.example.user_service.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository repo;
+
+    public UserService(UserRepository repo) {
+        this.repo = repo;
+    }
+
+    public User create(User user) {
+        return repo.save(user);
+    }
+
+    public List<User> getAll() {
+        return repo.findAll();
+    }
+
+    public User getById(Long id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+}

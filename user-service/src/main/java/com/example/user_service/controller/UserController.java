654984ad76f64939d579698e88cfe9f2e3b227f1
Hafsa FAIZ <h.faiz0872@uca.ac.ma>
2026-05-06 23:30:10 +0100

    message

--- user-service/src/main/java/com/example/user_service/controller/UserController.java
+++ user-service/src/main/java/com/example/user_service/controller/UserController.java
@@ -3,7 +3,11 @@ package com.example.user_service.controller;
 import com.example.user_service.dto.UserRequestDTO;
 import com.example.user_service.dto.UserResponseDTO;
 import com.example.user_service.service.UserService;
+
+import io.swagger.v3.oas.annotations.Operation;
+
 import jakarta.validation.Valid;
+
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,27 +22,32 @@ public class UserController {
         this.service = service;
     }
 
+    @Operation(summary = "Créer un utilisateur")
     @PostMapping
     public UserResponseDTO create(@Valid @RequestBody UserRequestDTO dto) {
         return service.create(dto);
     }
 
+    @Operation(summary = "Récupérer tous les utilisateurs")
     @GetMapping
     public List<UserResponseDTO> getAll() {
         return service.getAll();
     }
 
+    @Operation(summary = "Récupérer un utilisateur par ID")
     @GetMapping("/{id}")
     public UserResponseDTO getById(@PathVariable Long id) {
         return service.getById(id);
     }
 
+    @Operation(summary = "Modifier un utilisateur")
     @PutMapping("/{id}")
     public UserResponseDTO update(@PathVariable Long id,
                                   @RequestBody UserRequestDTO dto) {
         return service.update(id, dto);
     }
 
+    @Operation(summary = "Supprimer un utilisateur")
     @DeleteMapping("/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);

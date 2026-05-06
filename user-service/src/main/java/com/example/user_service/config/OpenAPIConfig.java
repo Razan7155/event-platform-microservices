cb4afa011f89215cc731b4cc24834ce3c18c38da
Hafsa FAIZ <h.faiz0872@uca.ac.ma>
2026-05-07 00:34:34 +0100

    message

--- user-service/src/main/java/com/example/user_service/config/OpenAPIConfig.java
+++ user-service/src/main/java/com/example/user_service/config/OpenAPIConfig.java
@@ -1,18 +1,40 @@
 package com.example.user_service.config;
 
-import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.*;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenAPIConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
         return new OpenAPI()
+
+                // INFO API
                 .info(new Info()
                         .title("User Service API")
                         .version("1.0")
-                        .description("API de gestion des utilisateurs"));
+                        .description("API de gestion des utilisateurs"))
+
+                // JWT SECURITY
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )
+                );
     }
 }

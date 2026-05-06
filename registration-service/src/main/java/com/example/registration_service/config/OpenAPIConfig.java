65775af6ca43f5c6e97470330d66a9d5c1d3a979
Hafsa FAIZ <h.faiz0872@uca.ac.ma>
2026-05-07 00:41:50 +0100

    message

--- registration-service/src/main/java/com/example/registration_service/config/OpenAPIConfig.java
+++ registration-service/src/main/java/com/example/registration_service/config/OpenAPIConfig.java
@@ -2,6 +2,9 @@ package com.example.registration_service.config;
 
 import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 import org.springframework.context.annotation.*;
 
 @Configuration
@@ -10,9 +13,25 @@ public class OpenAPIConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                // INFO API
                 .info(new Info()
-                        .title("egistration Service API")
+                        .title("User Service API")
                         .version("1.0")
-                        .description("API de gestion des inscriptions"));
+                        .description("API de gestion des utilisateurs"))
+
+                // JWT SECURITY
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+
+                                new SecurityScheme()
+                                        .name("bearerAuth")
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        )  
+                
+        );
     }
 }

f7e380bd37d979cc6f858abd35668cb362e13b68
Hafsa FAIZ <h.faiz0872@uca.ac.ma>
2026-05-08 23:58:33 +0100

    Ajout des modifications

--- api-gateway/src/main/java/com/example/apigateway/jwt/JwtService.java
+++ api-gateway/src/main/java/com/example/apigateway/jwt/JwtService.java
@@ -28,17 +28,8 @@ public class JwtService {
             return true;
 
         } catch (Exception e) {
+
             return false;
         }
-    }
-
-    public String extractEmail(String token) {
-
-        return Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody()
-                .getSubject();
     }
 }

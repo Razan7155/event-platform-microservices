76358fb76e0388e248e48ca2e4556aeb75550635
Hafsa FAIZ <h.faiz0872@uca.ac.ma>
2026-05-10 16:10:20 +0100

    Remove backend services from frontend branch

--- registration-service/src/main/java/com/example/registration_service/repository/RegistrationRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.registrationservice.repository;
-
-import com.example.registrationservice.model.Registration;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegistrationRepository extends JpaRepository<Registration, Long> {}
--- user-service/src/main/java/com/example/user_service/dto/UserDTO.java
+++
@@ -1,9 +0,0 @@
-package com.example.user_service.dto;
-
-public class UserDTO {
-   private Long id;
-    private String name;
-    private String email;
-
-    // getters & setters 
-}

4c9e6c61f5ddef7350ae2581fa2ec3cff249e96c
Hafsa FAIZ <h.faiz0872@uca.ac.ma>
2026-05-23 15:44:47 +0100

    updates

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,6 +8,8 @@ API.interceptors.request.use((config) => {
 
   const token = localStorage.getItem("token");
 
+  console.log("TOKEN SENT =", token);
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -2,30 +2,16 @@ import API from "./api";
 
 export const getUsers = async () => {
 
-  const token = localStorage.getItem("token");
-
-  const response = await API.get("/users", {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  });
+  const response =
+    await API.get("/users");
 
   return response.data;
 };
 
 export const createUser = async (data) => {
 
-  const token = localStorage.getItem("token");
-   
-  const response = await API.post(
-    "/users",
-    data,
-    {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
-  );
+  const response =
+    await API.post("/users", data);
 
   return response.data;
 };
